eebedb54ed8a71875f39f692cdae5f7d5815cee0
Tuyen Nguyen <tnguyen5822@gmail.com>
2025-12-20 23:33:39 -0500

    Time: 0 ms (100%), Space: 17.7 MB (98.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,28 +1,27 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        # find the pivot
-        left, right = 0, len(nums)-1
-
-        while left <= right:
-            mid = (left + right)//2
-            if nums[mid] == target:
-                return mid
-            #check if sorted in left half
-            if nums[left] <= nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            else:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def iforgotsearchname(arr):
+            '''
+            So we split into two, we have right, left and middle.
+            '''
+            left, right = 0, len(arr)-1
+            while left <= right:
+                mid = (left+right)//2
+                if arr[mid] == target:
+                    return mid
+                if arr[mid] >= arr[right]: #left side sorted, right side not
+                    if arr[left] <= target <= arr[mid]:
+                        right = mid -1
+                    else:
+                        left = mid + 1
+                else: #right side sorted
+                    if arr[mid] <= target <= arr[right]:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+            
+            return -1
         
-        return -1
+        return iforgotsearchname(nums)
+                    
         
